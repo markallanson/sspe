fbdc69e218a71e984982a39fc36de19b7cf56f90
markallanson <mark@markallanson.net>
2009-11-14 21:33:21 +0000

    Change to sample pachube script

--- Publishers/SamplePachube.py
+++ Publishers/SamplePachube.py
@@ -6,8 +6,8 @@ from System.Xml import XmlDocument
 from System.Diagnostics import Trace
 
 url = "http://pachube.com/api/"
-apiKey = "40ab667a92d6f892fef6099f38ad5eb31e619dffd793ff8842ae3b00eaf7d7cb"
-environmentId = 2065
+apiKey = "<Your-Pachube-Api-Key-Here>"
+environmentId = -1
 
 def Publish(topic, data):
     ms = MemoryStream()
